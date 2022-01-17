8a05d665cadcc0e883a25053decec20f8312cf80
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-17 14:11:59 -0300

    Retirado os using sem uso na classe TaxPayer e implementada a classe Individual

---
+++ ExFixClassesAbstratas/Entities/Individual.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ExFixClassesAbstratas.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) 
+            :base(name,anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            if(AnualIncome > 20000.00)
+            {
+                return AnualIncome * 0.15;
+            }
+            else
+            {
+                return (AnualIncome * 0.25) - (HealthExpenditures * 0.5);
+            }
+        }
+    }
+
+}
