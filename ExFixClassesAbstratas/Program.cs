41d0d8f1e00c89a04d9486b56301468a00076892
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-17 13:54:54 -0300

    Iniciando o desenvolvimento do Exercício de fixação sobre classes abstratas

---
+++ ExFixClassesAbstratas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExFixClassesAbstratas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
