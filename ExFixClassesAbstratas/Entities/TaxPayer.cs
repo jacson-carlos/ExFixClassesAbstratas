9ba36c9374574b954bcd88105164f3971057af48
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-17 14:01:33 -0300

    Criada a pasta Entities e implementada a classe TaxPayer

---
+++ ExFixClassesAbstratas/Entities/TaxPayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFixClassesAbstratas.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+
+    }
+}
